baf0069c889b904afe2959a8ded1b91d1452648c
Michał Maj <michalpmaj@gmail.com>
2023-03-24 19:39:15 +0100

    Variables and Constants

---
+++ 02 Variables and Constants/01 Variables in C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Declares a variable current_amount
+  int current_amount;
+  // Initialize a variable current_amount to 100
+  current_amount = 100;
+  // Prints the value of current_amount
+  cout << "Your current amount is: " << current_amount << endl;
+  // Updates the value of current_amount
+  current_amount = 120;
+  // Prints the updated value of current_amount
+  cout << "Your current amount is: " << current_amount << endl;
+}
