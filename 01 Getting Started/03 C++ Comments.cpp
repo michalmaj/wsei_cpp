4593e2ab2f18cb80b1a2f8e467bc621619aa35f9
Michał Maj <michalpmaj@gmail.com>
2023-03-24 19:30:29 +0100

    C++ Comment

---
+++ 01 Getting Started/03 C++ Comments.cpp
@@ -0,0 +1,35 @@
+// Single line comments
+#include <iostream>
+
+using namespace std;
+int main() {
+  // I am a single-line comment
+  // Compiler will ignore me
+  // cout << "Hey"
+  cout << "Hello World";
+}
+
+// Multiline comments
+#include <iostream>
+
+using namespace std;
+int main() {
+  /* I am a multi-line comment
+  Compiler will ignore me
+  cout << "Hey" */
+  cout << "Hello World";
+}
+
+// White spaces
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+
+
+  cout    <<   "Hello World";
+
+}
