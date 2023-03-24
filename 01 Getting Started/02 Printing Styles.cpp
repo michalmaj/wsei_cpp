69906904c046bee734ca13ddd16fb16a2b24b540
Michał Maj <michalpmaj@gmail.com>
2023-03-24 19:26:30 +0100

    Printing styles

---
+++ 01 Getting Started/02 Printing Styles.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//   cout << "Hello ";
+//   cout << "World";
+// }
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello " << endl;
+  cout << "World";
+}
+
