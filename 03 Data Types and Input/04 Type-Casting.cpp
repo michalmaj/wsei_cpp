ebd0a400403d1d5f5a86ba8deadf162b21041c67
Michał Maj <michalpmaj@gmail.com>
2023-03-24 20:41:07 +0100

    Data Types and Input

---
+++ 03 Data Types and Input/04 Type-Casting.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Stores floating point value in variable of type int
+  int int_value = 13.9;
+  // Displays the value of variable
+  cout << int_value << endl;
+
+
+
+  // Initializes a variable of char data type
+  char character = 'A';
+  // Declares a variable of int type
+  int ASCII;
+  // Converts char data type into int explicitly
+  ASCII = (int) character; // Old C-style casting
+  // Prints value of variable
+  cout << "ASCII value = " << ASCII << endl;
+
+}
