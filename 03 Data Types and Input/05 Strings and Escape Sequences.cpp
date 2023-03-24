4deb691ab6f1260b64d84c405313cb399d88a166
Michał Maj <michalpmaj@gmail.com>
2023-03-24 20:47:50 +0100

    Data Types and Input

---
+++ 03 Data Types and Input/05 Strings and Escape Sequences.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Initialize a string variable
+  string text = "Hey12345";
+  // Displays value of string variable
+  cout << text << endl;
+
+
+  // Initialize string variable with text and escape characters
+  text = "Hello\nI\tam\tJohn";
+  // Displays value of string variable
+  cout << text << endl;
+}
