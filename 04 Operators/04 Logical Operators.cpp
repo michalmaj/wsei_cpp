df2ceb635ef959d599e57570f3bd50d76da14b46
Michał Maj <michalpmaj@gmail.com>
2023-03-24 21:11:58 +0100

    Operators

---
+++ 04 Operators/04 Logical Operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  bool operand1 = 2 > 3; // Will be false
+  bool operand2 = true;
+  cout << "Values of operands are:";
+  
+  cout << "operand1 = " << operand1 << " , operand2 = " << operand2 << endl;
+  cout << "operand1 && operand2 = " << (operand1 && operand2) << endl;
+  cout << "operand1 || operand2 = " << (operand1 || operand2) << endl;
+  cout << "!operand1 = " << (!operand1) << endl;
+  cout << "!operand2 = " << (!operand2) << endl;
+  
+  return 0;
+}
