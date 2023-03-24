30719f7ea2149aa480b2e591c059618a56e508ed
Michał Maj <michalpmaj@gmail.com>
2023-03-24 19:46:47 +0100

    Variables and Constants

---
+++ 02 Variables and Constants/02 Identifiers in C++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Example program with valid identifiers
+    int number1;
+    int _number;
+    int number;
+
+    // Example program with invalid identifiers (The program given below will generate an error.)
+    // int 1;
+    // int number 1;
+    // int return;
+
+}
