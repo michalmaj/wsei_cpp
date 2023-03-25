c974604527e019e8af708c522cb3ef0f8ba24aa3
Michał Maj <michalpmaj@gmail.com>
2023-03-25 06:37:26 +0100

    05 Conditional Statements

---
+++ 05 Conditional Statements/01a If Statement.cpp
@@ -0,0 +1,16 @@
+// Example program when the condition is true
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Initialize money to 21
+  int money = 21;
+  // If condition
+  if (money >= 20) {
+    // If body  
+    cout << "You can buy a watch" << endl;
+  }
+  // Exit 
+  return 0;
+}
---
+++ 05 Conditional Statements/01b If Statement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Initialize variable money
+  int money = 9;
+  // if condition
+  if (money >= 20) {
+    // if body  
+    cout << "You can buy a watch" << endl;
+  }
+  // exit
+  return 0;
+}
