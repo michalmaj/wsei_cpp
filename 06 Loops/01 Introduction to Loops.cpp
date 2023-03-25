fcbf8e452550af1b90b19b7fb54690d255bae84c
Michał Maj <michalpmaj@gmail.com>
2023-03-25 07:16:14 +0100

    Loops

---
+++ 06 Loops/01 Introduction to Loops.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int number = 0;
+  cout << number++ << endl;
+  cout << number++ << endl;
+  cout << number++ << endl;
+  cout << number++ << endl;
+  cout << number++ << endl;
+  return 0;
+}
