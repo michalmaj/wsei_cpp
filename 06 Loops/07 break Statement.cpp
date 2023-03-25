ac5b5b097ec8e16529e2b3a76d28064cc9fea518
Michał Maj <michalpmaj@gmail.com>
2023-03-25 07:44:12 +0100

    Loops

---
+++ 06 Loops/07 break Statement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Initialize variable icecream
+  int icecream;
+  // for loop start
+  for (icecream = 5; icecream > 0; icecream--) {
+    // loop body
+    cout << "Number of free ice-creams = " << icecream << endl;
+    // break statement
+    if (icecream == 2) {
+      break;
+    }
+    cout << "Buy an icecream" << endl;
+  }
+  // Exit loop
+  cout << "Sorry! We ran out of ice-cream" << endl;
+  return 0;
+}
