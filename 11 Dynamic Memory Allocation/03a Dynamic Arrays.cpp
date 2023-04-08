d6992d50c1ef3a94c878705ef5bb4cd40c30455d
Michał Maj <michalpmaj@gmail.com>
2023-04-08 20:42:09 +0200

    Dynamic Arrays

---
+++ 11 Dynamic Memory Allocation/03a Dynamic Arrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int size = 5;
+  //Declare dynamic array
+  int *Array = new int[size];
+  //Initialize dynamic array
+  for(int i = 0 ; i< size; i++){
+    Array[i] = i;
+  }   
+  //Prints dynamic array
+  for(int i = 0 ; i< size; i++){
+    cout << Array[i] << " ";
+  } 
+  cout << endl;
+ // Deletes a memory allocated to dynamic array
+   delete[] Array;
+}
