1de9f9b9ef6a5894904ab976b80b363c965e3477
Michał Maj <michalpmaj@gmail.com>
2023-04-08 20:50:00 +0200

    Declaring Structure Variables in C++

---
+++ 12 Structures/02a Declaring Structure Variables in C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+// Student structure
+struct Student {
+  string name;
+  int roll_number;
+  int marks;
+};
+// main function
+int main() {
+  Student s1, s2, s3;
+  return 0;
+}
---
+++ 12 Structures/02b Declaring Structure Variables in C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+// Student structure
+struct Student {
+  string name;
+  int roll_number;
+  int marks;
+} s1, s2, s3;
+// main function
+int main() {
+  return 0;
+}
