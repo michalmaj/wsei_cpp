40d1f6e22b6c2d52583c8fb0ad3777b902cadffd
Michał Maj <michalpmaj@gmail.com>
2023-04-08 20:47:40 +0200

    Defining Structure in C++

---
+++ 12 Structures/01 Defining Structure in C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+// Student structure
+struct Student {
+  string name;
+  int roll_number;
+  int marks;
+};
+// main function
+int main() {
+
+  return 0;
+}
