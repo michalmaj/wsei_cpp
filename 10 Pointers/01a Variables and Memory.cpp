6f0158ede7b6475032700d5d164d878750f9adcc
Michał Maj <michalpmaj@gmail.com>
2023-04-08 20:08:39 +0200

    Variables and Memory

---
+++ 10 Pointers/01a Variables and Memory.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Declares a variable John of type int
+  int John;
+}
---
+++ 10 Pointers/01b Variables and Memory.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Declares a variable John of type int
+  int John;
+  // Stores 10 in variable John
+  John = 10;
+}
