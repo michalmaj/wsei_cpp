b901eb65dd052bfe4b1c0921e7f730c44a1415b5
Michał Maj <michalpmaj@gmail.com>
2023-04-14 18:04:42 +0200

    Convert Double Value into Integer

---
+++ 13 Exercises/05 Convert Double Value into Integer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Initialize a variable of type double
+  double double_value = 70.968;
+  // Prints value of variable 
+  cout << "double_value = " << double_value << endl;
+  // Declare a variable of type int
+  int integer_value;
+  // Assign double value to variable of integer type
+  integer_value = double_value;
+  // Prints value of variable
+  cout << "integer_value = " << integer_value << endl;
+}
