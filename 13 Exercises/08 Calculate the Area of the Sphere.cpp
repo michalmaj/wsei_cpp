f55361d6725a094f738f260ac1802da588bc6d24
Michał Maj <michalpmaj@gmail.com>
2023-04-14 18:19:21 +0200

    Calculate the Area of the Sphere

---
+++ 13 Exercises/08 Calculate the Area of the Sphere.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <numbers>
+
+using namespace std;
+
+int main() {
+// Initialize a variable r    
+double r =  10.1;
+// Prints value of r
+cout << "r = " << r << endl;
+// Initialize a constant pi
+const double pi = 3.14;
+// Declares a variable area
+double area;
+// Find the area using a given formula
+area = 4*pi*r*r;
+// Prints value of area
+cout << "area = " << area << endl;
+}
